d3cb5725cf873532669de88389f3ba59b71ff9d1
Dave Kellam <dave@eightface.com>
2014-01-31 20:53:40 -0500

    Calling appropriate admin page now

--- flickrrss.php
+++ flickrrss.php
@@ -185,7 +185,7 @@ if ( ! class_exists( 'flickrRSS' ) ) {
 	
 		function setupSettingsPage() {
 			if ( function_exists( 'add_options_page') ) {
-				add_options_page( 'flickrRSS Settings', 'flickrRSS', 'manage_options', 'flickrrss-settingspage.php', array( &$this, 'printSettingsPage' ) );
+				add_options_page( 'flickrRSS Settings', 'flickrRSS', 'manage_options', 'admin.php', array( &$this, 'printSettingsPage' ) );
 			}
 		}
 	
